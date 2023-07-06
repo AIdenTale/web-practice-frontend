eefac8c86c4eefdf310d7afa49de3910955dac94
Nikitin Ruslan <mystudychannel.0@gmail.com>
2023-07-06 16:40:22 +0300

    hero fix 2

--- src/pages/general.jsx
+++ src/pages/general.jsx
@@ -1,9 +1,29 @@
-import HeroGeneral from "../components/hero/hero-general";
+import Button from "../components/UI/button";
 
 function General() {
-  return (  
+  return (
     <main className="General">
-      <HeroGeneral/>      
+      <div className="Hero Hero__general">
+        <div className="overlay">
+          <div className="Hero-content container">
+            <div className="title-bg">
+              <h1 className="title">Game Title</h1>
+            </div>
+
+            <p className="body">Пошаговая стратегия — жанр компьютерных игр, где участники делают ходы по очереди.
+              Заказать услугу Разработка пошаговых стратегий можно у специалистов компании DreamCraft Studio.
+              В результате Вы получите прибыльный продукт.
+            </p>
+
+            <Button>Присоединиться</Button>
+          </div>
+        </div>
+      </div>
+      <section className="General-characters">
+        <div className="General-content"></div>
+        <div className="bg-gradient"></div>
+        <div className="bg"></div>
+      </section>
     </main>
   );
 }
